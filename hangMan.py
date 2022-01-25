3e246e0d5b323647d7fc4330290788c9d322cb04
Torsten <apps+github@torsten-albrecht.de>
2022-01-25 18:19:17 +0100

    version 1.0

--- hangMan.py
+++ hangMan.py
@@ -1,12 +1,41 @@
 import random
 
 print("Hangman in Python :-)")
+
+# Speichern von erratenen Buchstaben
 erraten = []
-woerter = 'Pause Ende Hallo'.split()
+
+# Eingabe Benutzer
+benutzereingabe = ""
+
+# Woerterliste
+woerter = 'Hund Katze Maus Fliege Vogel Giraffe Freddie'.split()
 ratewort = random.choice(woerter)
-
 print(ratewort)
-for buchstaben in ratewort:
-    print(" ", end='_')
 
-    print("End")
+# Buchstaben in Wort ersetzen
+for buchstaben in ratewort:
+    #print(" ", end='_')
+    erraten.append('_')
+
+while benutzereingabe != "bye":
+    for ausgabe in erraten:
+        print(ausgabe, end=' ')
+    print() # fuer nächste Zeile
+    benutzereingabe = input("Dein erster Buchstabe: ")
+    x = 0
+    
+    # Ueberpruefung, ob Buchstaben in Wort
+    for buchstaben in ratewort:
+        if buchstaben.lower() == benutzereingabe.lower():
+            # print("richtig")
+            erraten[x] = buchstaben
+        x += 1
+    print()
+
+    # Gewonnen?
+    if '_' in erraten:
+        print("noch nicht gewonnen")
+    else:
+        print("Gewonnen")
+        break
