c0c7c24415c5bb5c2f0cebd6b7f4a577d760a6ac
Torsten <apps+github@torsten-albrecht.de>
2022-01-25 19:01:42 +0100

    1.0

---
+++ nummernRaten.py
@@ -0,0 +1,28 @@
+import random
+
+runde = 0
+aktiv = True
+computerzahl = random.randint(1,100)
+#print(computerzahl)
+
+while aktiv:
+    runde = runde + 1
+    print("Runde. " + str(runde))
+    benutzereingabe = input("Bitte eine Zahl eingeben, zum beenden ende tippen: ")
+    benutzereingabe = int(benutzereingabe)
+
+    if benutzereingabe == computerzahl:
+        print("gewonnen")
+    elif benutzereingabe < computerzahl:
+        print("Zahl ist zu klein")
+    else:
+        benutzereingabe > computerzahl
+        print("Zahl ist zu groß")
+    
+    if runde == 7:
+        print("Leider verloren")
+        print("die gesuchte Zahl war: " + str(computerzahl))
+        aktiv = False
+    if benutzereingabe == "ende":
+        aktiv = False
+print("und tschüss")
